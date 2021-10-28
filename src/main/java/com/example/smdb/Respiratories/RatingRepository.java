390340688dd86b7b11973b966b6888c58450101a
Sara-Maged <83797445+Sara-Maged@users.noreply.github.com>
2021-10-28 03:29:11 +0200

    Update RatingRepository.java

--- src/main/java/com/example/smdb/Respiratories/RatingRepository.java
+++ src/main/java/com/example/smdb/Respiratories/RatingRepository.java
@@ -8,13 +8,6 @@ import java.util.List;
 
 public interface RatingRepository extends JpaRepository<RatingEntity, Integer> {
 <<<<<<< HEAD
-    //List<RatingEntity> findAllByMovieEntity_IdWithin(int id);
-
-//    List<RatingEntity> findAllByRatingIdUserIdWithin();
-//    List<RatingEntity> getAllByRatingIdUserIdWithin(int id);
-
-//    @Query("select r from RatingEntity r where r.ratingId.userId = id order by r.rating")
-//    List<RatingEntity> findByUserEntityIdWithinOrderByRating(Integer id);
 
     @Query("select ratnigEntity from RatingEntity ratnigEntity where ratnigEntity.ratingId.userId = ?1 and ratnigEntity.rating = (select max (r.rating) from ratnigEntity r)")
     RatingEntity findUserTopRatedMovie(int userid);
